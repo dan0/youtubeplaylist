ef9b4a189f44296ee4a9d727932d8885bfb4f7c9
Dan <dan@allbutlost.com>
2011-05-16 21:36:24 +0100

    allows new youtube iframe code + mixed galleries

--- jquery.youtubeplaylist.js
+++ jquery.youtubeplaylist.js
@@ -5,15 +5,22 @@
 //
 //		v1.1 - updated to allow fullscreen 
 //			 - thanks Ashraf for the request
+//      v1.2 - updated to allow for mixed yt/image galleries
+//           - for Markus Thyberg
 //-------------------------------------------------
+
+String.prototype.startsWith = function(str){
+    return (this.indexOf(str) === 0);
+}
+
 
 jQuery.fn.ytplaylist = function(options) {
  
   // default settings
   var options = jQuery.extend( {
     holderId: 'ytvideo',
-	playerHeight: '300',
-	playerWidth: '450',
+	playerHeight: 300,
+	playerWidth: 450,
 	addThumbs: false,
 	thumbSize: 'small',
 	showInline: false,
@@ -24,7 +31,7 @@ jQuery.fn.ytplaylist = function(options) {
  
   return this.each(function() {
 							
-   		var selector = $(this);
+   		var $el = $(this);
 		
 		var autoPlay = "";
 		var showRelated = "&rel=0";
@@ -34,8 +41,7 @@ jQuery.fn.ytplaylist = function(options) {
 		if(options.allowFullScreen) fullScreen = "&fs=1"; 
 		
 		//throw a youtube player in
-		function play(id)
-		{
+		function playOld(id) {
 		   var html  = '';
 	
 		   html += '<object height="'+options.playerHeight+'" width="'+options.playerWidth+'">';
@@ -52,8 +58,15 @@ jQuery.fn.ytplaylist = function(options) {
 		   html += '</object>';
 			
 		   return html;
-		   
 		};
+		
+		
+		function playNew (id) {
+		  var html = '';
+		  html += '<iframe width="'+ options.playerWidth +'" height="'+ options.playerHeight +'"';
+		  html += ' src="http://www.youtube.com/embed/'+ id +'" frameborder="0"';
+		  hml += ' allowfullscreen></iframe>';
+		}
 		
 		
 		//grab a youtube id from a (clean, no querystring) url (thanks to http://jquery-howto.blogspot.com/2009/05/jyoutube-jquery-youtube-thumbnail.html)
@@ -65,43 +78,109 @@ jQuery.fn.ytplaylist = function(options) {
 		
 		
 		//load inital video
-		var firstVid = selector.children("li:first-child").addClass("currentvideo").children("a").attr("href");
-		$("#"+options.holderId+"").html(play(youtubeid(firstVid)));
+		var firstVid = $el.children("li:first-child").addClass("currentvideo").children("a").attr("href");
+		$("#"+options.holderId+"").html(playOld(youtubeid(firstVid)));
+		
+		//
+		$el.children('li').each(function() {
+            $(this).find('a').each(function() {
+                var thisHref = $(this).attr('href');
+                
+                //old-style youtube links
+                if (thisHref.startsWith('http://www.youtube.com')) {
+                    $(this).addClass('yt-vid');
+                    $(this).data('yt-id', youtubeid(thisHref) );
+                }
+                //new style youtu.be links
+                else if (thisHref.startsWith('http://youtu.be')) {
+                    $(this).addClass('yt-vid');
+                    var id = thisHref.substr(thisHref.lastIndexOf("/") + 1);
+                    $(this).data('yt-id', id );
+                }
+                else {
+                    //must be an image link (naive)
+                    $(this).addClass('img-link');
+                }
+                
+               // alert(thisHref);
+            });
+		});
+		
 		
 		//load video on request
-		selector.children("li").children("a").click(function() {
+		$el.children("li").children("a.yt-vid").click(function() {
 			
 			if(options.showInline) {
 				$("li.currentvideo").removeClass("currentvideo");
-				$(this).parent("li").addClass("currentvideo").html(play(youtubeid($(this).attr("href"))));
+				$(this).parent("li").addClass("currentvideo").html(playOld($(this).data("yt-id")));
 			}
 			else {
-				$("#"+options.holderId+"").html(play(youtubeid($(this).attr("href"))));
+				$("#"+options.holderId+"").html(playOld($(this).data("yt-id")));
 				$(this).parent().parent("ul").find("li.currentvideo").removeClass("currentvideo");
 				$(this).parent("li").addClass("currentvideo");
-			}
-															 
-			
-			
+			}	
 			return false;
 		});
+
+		$el.find("a.img-link").click(function() {
+		    var $img = $('<img/>');
+		    $img.attr({
+		        src:$(this).attr('href'),
+		        width : options.playerWidth });
+		    
+		    if(options.showInline) {
+		        $("li.currentvideo").removeClass("currentvideo");
+		        $(this).parent("li").addClass("currentvideo").html($img);
+	        }
+	        else {
+	            
+	            $("#"+options.holderId+"").html($img);
+	            var contentGap = options.playerHeight - parseInt($img.height(), 10);
+	            $img.css({
+	                position: 'relative',
+	                top: parseInt(contentGap/2, 10)
+	            })
+				$(this).closest("ul").find("li.currentvideo").removeClass("currentvideo");
+				$(this).parent("li").addClass("currentvideo");
+				
+	        }
+            
+		    return false;
+	    });
+		
 		
 		//do we want thumns with that?
 		if(options.addThumbs) {
 			
-			selector.children().each(function(i){
-											  
+			$el.children().each(function(i){
+				
+				//replace first link
+				var $link = $(this).find('a:first');
 				var replacedText = $(this).text();
 				
-				if(options.thumbSize == 'small') {
-					var thumbUrl = "http://img.youtube.com/vi/"+youtubeid($(this).children("a").attr("href"))+"/2.jpg";
+				if ($link.hasClass('yt-vid')) {
+				    
+				    if(options.thumbSize == 'small') {
+    					var thumbUrl = "http://img.youtube.com/vi/"+$link.data("yt-id")+"/2.jpg";
+    				}
+    				else {
+    					var thumbUrl = "http://img.youtube.com/vi/"+$link.data("yt-id")+"/0.jpg";
+    				}
+
+    				var thumbHtml = "<img src='"+thumbUrl+"' alt='"+replacedText+"' />";
+    				$link.empty().html(thumbHtml+replacedText).attr("title", replacedText);
+				    
 				}
 				else {
-					var thumbUrl = "http://img.youtube.com/vi/"+youtubeid($(this).children("a").attr("href"))+"/0.jpg";
+				    //is an image link
+				    var $img = $('<img/>').attr('src',$link.attr('href'));
+				    $link.empty().html($img).attr("title", replacedText);
 				}
 				
 				
-				$(this).children("a").empty().html("<img src='"+thumbUrl+"' alt='"+replacedText+"' />"+replacedText).attr("title", replacedText);
+				
+				
+				
 				
 			});	
 			
