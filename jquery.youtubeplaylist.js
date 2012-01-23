bdeae668fb4605d953134eec9ddcfbcf520ddd14
Dan Drayne <dan@allbutlost.com>
2012-01-23 19:15:43 +0100

    formatting fixes, added license

--- jquery.youtubeplaylist.js
+++ jquery.youtubeplaylist.js
@@ -1,73 +1,69 @@
-//-------------------------------------------------
-//		youtube playlist jquery plugin
-//		Created by dan@geckonm.com
-//		www.geckonewmedia.com
-//
-//		v1.1 - updated to allow fullscreen 
-//			 - thanks Ashraf for the request
-//      v1.2 - updated to allow for mixed yt/image galleries
-//           - for Markus Thyberg
-//-------------------------------------------------
+/**
+ *	jquery.youtubeplaylist.js
+ *  dan@allbutlost.com
+ *
+ *	Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ */
 
 String.prototype.startsWith = function(str){
     return (this.indexOf(str) === 0);
-}
+};
 
 
 jQuery.fn.ytplaylist = function(options) {
  
-  // default settings
-  var options = jQuery.extend( {
-    holderId: 'ytvideo',
-	playerHeight: 300,
-	playerWidth: 450,
-	addThumbs: false,
-	thumbSize: 'small',
-	showInline: false,
-	autoPlay: true,
-	showRelated: true,
-	allowFullScreen: false
-  },options);
+	// default settings
+	var options = jQuery.extend({
+		holderId: 'ytvideo',
+		playerHeight: 300,
+		playerWidth: 450,
+		addThumbs: false,
+		thumbSize: 'small',
+		showInline: false,
+		autoPlay: true,
+		showRelated: true,
+		allowFullScreen: false
+	},options);
  
-  return this.each(function() {
+	return this.each(function() {
 							
-   		var $el = jQuery(this);
+		var $el = jQuery(this);
 		
 		var autoPlay = "";
 		var showRelated = "&rel=0";
 		var fullScreen = "";
-		if(options.autoPlay) autoPlay = "&autoplay=1"; 
-		if(options.showRelated) showRelated = "&rel=1"; 
-		if(options.allowFullScreen) fullScreen = "&fs=1"; 
+		if(options.autoPlay) autoPlay = "&autoplay=1";
+		if(options.showRelated) showRelated = "&rel=1";
+		if(options.allowFullScreen) fullScreen = "&fs=1";
 		
 		//throw a youtube player in
 		function playOld(id) {
-		   var html  = '';
+			var html  = '';
 	
-		   html += '<object height="'+options.playerHeight+'" width="'+options.playerWidth+'">';
-		   html += '<param name="movie" value="http://www.youtube.com/v/'+id+autoPlay+showRelated+fullScreen+'"> </param>';
-		   html += '<param name="wmode" value="transparent"> </param>';
-		   if(options.allowFullScreen) { 
-		   		html += '<param name="allowfullscreen" value="true"> </param>'; 
-		   }
-		   html += '<embed src="http://www.youtube.com/v/'+id+autoPlay+showRelated+fullScreen+'"';
-		   if(options.allowFullScreen) { 
-		   		html += ' allowfullscreen="true" '; 
-		   	}
-		   html += 'type="application/x-shockwave-flash" wmode="transparent"  height="'+options.playerHeight+'" width="'+options.playerWidth+'"></embed>';
-		   html += '</object>';
+			html += '<object height="'+options.playerHeight+'" width="'+options.playerWidth+'">';
+			html += '<param name="movie" value="http://www.youtube.com/v/'+id+autoPlay+showRelated+fullScreen+'"> </param>';
+			html += '<param name="wmode" value="transparent"> </param>';
+			if(options.allowFullScreen) {
+				html += '<param name="allowfullscreen" value="true"> </param>';
+			}
+			html += '<embed src="http://www.youtube.com/v/'+id+autoPlay+showRelated+fullScreen+'"';
+			if(options.allowFullScreen) {
+				html += ' allowfullscreen="true" ';
+			}
+			html += 'type="application/x-shockwave-flash" wmode="transparent"  height="'+options.playerHeight+'" width="'+options.playerWidth+'"></embed>';
+			html += '</object>';
 			
-		   return html;
-		};
+			return html;
+		}
 		
 		
 		function playNew (id) {
-		  var html = '';
-		  html += '<iframe width="'+ options.playerWidth +'" height="'+ options.playerHeight +'"';
-		  html += ' src="http://www.youtube.com/embed/'+ id +'?wmode=opaque' + showRelated + '" frameborder="0"';
-		  html += ' allowfullscreen></iframe>';
+			var html = '';
+			html += '<iframe width="'+ options.playerWidth +'" height="'+ options.playerHeight +'"';
+			html += ' src="http://www.youtube.com/embed/'+ id +'?wmode=opaque' + showRelated + '" frameborder="0"';
+			html += ' allowfullscreen></iframe>';
 
-		  return html;
+			return html;
 		}
 		
 		
@@ -76,33 +72,31 @@ jQuery.fn.ytplaylist = function(options) {
 			var ytid = url.match("[\\?&]v=([^&#]*)");
 			ytid = ytid[1];
 			return ytid;
-		};
+		}
 		
 		
 		
 		//
 		$el.children('li').each(function() {
-            jQuery(this).find('a').each(function() {
-                var thisHref = jQuery(this).attr('href');
-                
-                //old-style youtube links
-                if (thisHref.startsWith('http://www.youtube.com')) {
-                    jQuery(this).addClass('yt-vid');
-                    jQuery(this).data('yt-id', youtubeid(thisHref) );
-                }
-                //new style youtu.be links
-                else if (thisHref.startsWith('http://youtu.be')) {
-                    jQuery(this).addClass('yt-vid');
-                    var id = thisHref.substr(thisHref.lastIndexOf("/") + 1);
-                    jQuery(this).data('yt-id', id );
-                }
-                else {
-                    //must be an image link (naive)
-                    jQuery(this).addClass('img-link');
-                }
-                
-               // alert(thisHref);
-            });
+			jQuery(this).find('a').each(function() {
+				var thisHref = jQuery(this).attr('href');
+
+				//old-style youtube links
+				if (thisHref.startsWith('http://www.youtube.com')) {
+					jQuery(this).addClass('yt-vid');
+					jQuery(this).data('yt-id', youtubeid(thisHref) );
+				}
+				//new style youtu.be links
+				else if (thisHref.startsWith('http://youtu.be')) {
+					jQuery(this).addClass('yt-vid');
+					var id = thisHref.substr(thisHref.lastIndexOf("/") + 1);
+					jQuery(this).data('yt-id', id );
+				}
+				else {
+					//must be an image link (naive)
+					jQuery(this).addClass('img-link');
+				}
+			});
 		});
 		
 		
@@ -117,53 +111,53 @@ jQuery.fn.ytplaylist = function(options) {
 				jQuery("#"+options.holderId+"").html(playNew(jQuery(this).data("yt-id")));
 				jQuery(this).parent().parent("ul").find("li.currentvideo").removeClass("currentvideo");
 				jQuery(this).parent("li").addClass("currentvideo");
-			}	
+			}
 			return false;
 		});
 
 		$el.find("a.img-link").click(function() {
-		    var $img = jQuery('<img/>');
-		    $img.attr({
-		            src:jQuery(this).attr('href') })
-		        .css({
-		            display: 'none',
-		            position: 'absolute',
-		            left: '0px',
-		            top: '50%'});
+			var $img = jQuery('<img/>');
+				$img.attr({src:jQuery(this).attr('href') })
+					.css({
+						display: 'none',
+						position: 'absolute',
+						left: '0px',
+						top: '50%'});
 
-		    if(options.showInline) {
-		        jQuery("li.currentvideo").removeClass("currentvideo");
-		        jQuery(this).parent("li").addClass("currentvideo").html($img);
-	        }
-	        else {
-	            
-	            jQuery("#"+options.holderId+"").html($img);
-				jQuery(this).closest("ul").find("li.currentvideo").removeClass("currentvideo");
-				jQuery(this).parent("li").addClass("currentvideo");
+				if(options.showInline) {
+					jQuery("li.currentvideo").removeClass("currentvideo");
+					jQuery(this).parent("li").addClass("currentvideo").html($img);
+				}
+				else {
+					jQuery("#"+options.holderId+"").html($img);
+					jQuery(this).closest("ul").find("li.currentvideo").removeClass("currentvideo");
+					jQuery(this).parent("li").addClass("currentvideo");
+				}
+
+				//wait for image to load (webkit!), then set width or height
+				//based on dimensions of the image
+				setTimeout(function() {
+					if ( $img.height() < $img.width() ) {
+						$img.width(options.playerWidth).css('margin-top',parseInt($img.height()/-2, 10)).css({
+							height: 'auto'
+						});
+					}
+					else {
+						$img.css({
+							height: options.playerHeight,
+							width: 'auto',
+							top: '0px',
+							position: 'relative'
+						});
+					}
 				
-	        }
-            //wait for image to load (webkit!), then set width or height
-            //based on dimensions of the image
-            setTimeout(function() {
-                if ( $img.height() < $img.width() ) {
-                    $img.width(options.playerWidth).css('margin-top',parseInt($img.height()/-2, 10)).css({
-                        height: 'auto'
-                    });
-                }
-                else {
-                    $img.css({
-                        height: options.playerHeight,
-                        width: 'auto',
-                        top: '0px',
-                        position: 'relative'
-                    });
-                }
-                $img.fadeIn();
-            }, 100);
-            
-            
-		    return false;
-	    });
+					$img.fadeIn();
+				
+				}, 100);
+
+
+			return false;
+		});
 		
 		
 		//do we want thumns with that?
@@ -174,36 +168,32 @@ jQuery.fn.ytplaylist = function(options) {
 				//replace first link
 				var $link = jQuery(this).find('a:first');
 				var replacedText = jQuery(this).text();
-				
+				var thumbUrl;
 				if ($link.hasClass('yt-vid')) {
-				    
-				    if(options.thumbSize == 'small') {
-    					var thumbUrl = "http://img.youtube.com/vi/"+$link.data("yt-id")+"/2.jpg";
-    				}
-    				else {
-    					var thumbUrl = "http://img.youtube.com/vi/"+$link.data("yt-id")+"/0.jpg";
-    				}
+					if(options.thumbSize == 'small') {
+						thumbUrl = "http://img.youtube.com/vi/"+$link.data("yt-id")+"/2.jpg";
+					}
+					else {
+						thumbUrl = "http://img.youtube.com/vi/"+$link.data("yt-id")+"/0.jpg";
+					}
 
-    				var thumbHtml = "<img src='"+thumbUrl+"' alt='"+replacedText+"' />";
-    				$link.empty().html(thumbHtml+replacedText).attr("title", replacedText);
-				    
+					var thumbHtml = "<img src='"+thumbUrl+"' alt='"+replacedText+"' />";
+					$link.empty().html(thumbHtml+replacedText).attr("title", replacedText);
+
 				}
 				else {
-				    //is an image link
-				    var $img = jQuery('<img/>').attr('src',$link.attr('href'));
-				    $link.empty().html($img).attr("title", replacedText);
-				}	
+					//is an image link
+					var $img = jQuery('<img/>').attr('src',$link.attr('href'));
+					$link.empty().html($img).attr("title", replacedText);
+				}
 				
-			});	
+			});
 			
 		}
 		
 		//load inital video
 		var firstVid = $el.children("li:first-child").addClass("currentvideo").children("a").click();
-        
-			
-		
-   
-  });
- 
+
+	});
+
 };
